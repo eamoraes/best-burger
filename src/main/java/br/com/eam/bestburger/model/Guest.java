4d0f30208f2e10ad1d841fdf2e7143408295f3a2
Ederson Moraes <eamoraes@gmail.com>
2019-02-21 14:29:01 -0300

    Guest modified

--- src/main/java/br/com/eam/bestburger/model/Guest.java
+++ src/main/java/br/com/eam/bestburger/model/Guest.java
@@ -4,9 +4,10 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name="guests")
@@ -15,7 +16,8 @@ public class Guest implements Serializable {
 	private static final long serialVersionUID = 810869730976832078L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
 	private Long id;
 	
 	private String name;
