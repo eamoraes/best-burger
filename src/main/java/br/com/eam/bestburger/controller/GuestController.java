5a1aa794cc3fb84bddb76838b6669a3ff826f21e
Ederson Moraes <eamoraes@gmail.com>
2019-02-22 15:14:09 -0300

    edit and delete buttons

--- src/main/java/br/com/eam/bestburger/controller/GuestController.java
+++ src/main/java/br/com/eam/bestburger/controller/GuestController.java
@@ -1,9 +1,12 @@
 package br.com.eam.bestburger.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,15 +22,36 @@ public class GuestController {
 
 	@GetMapping
 	public String list(Model model) {
-		
 		model.addAttribute(new Guest());
 		model.addAttribute("guests", guestRepository.findAll());
-		return "GuestList";
+		return "guest/list";
 	}
 	
 	@PostMapping
 	public String save(Guest guest) {
 		this.guestRepository.save(guest);
 		return "redirect:/guests";
+	}
+	
+	@GetMapping("/{id}")
+	public String findOne(@PathVariable Long id) {
+		Optional<Guest> guest = this.guestRepository.findById(id);
+		
+		System.out.println(guest.get());
+
+		return "redirect:/guests";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Long id) {
+		
+		this.guestRepository.deleteById(id);
+		return "redirect:/guests";
+	}
+	
+	@GetMapping("/form")
+	public String form(Guest guest, Model model) {
+		model.addAttribute(guest);
+		return "guest/form";
 	}
 }
--- src/main/java/br/com/eam/bestburger/model/Guest.java
+++ src/main/java/br/com/eam/bestburger/model/Guest.java
@@ -48,4 +48,48 @@ public class Guest implements Serializable {
 		this.companionAmount = companionAmount;
 	}
 
+	@Override
+	public String toString() {
+		return "Guest [id=" + id + ", name=" + name + ", companionAmount=" + companionAmount + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((companionAmount == null) ? 0 : companionAmount.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Guest other = (Guest) obj;
+		if (companionAmount == null) {
+			if (other.companionAmount != null)
+				return false;
+		} else if (!companionAmount.equals(other.companionAmount))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+
 }
